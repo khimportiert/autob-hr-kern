0f6c362de975aaabf6d39acfe96e01d53d3fa749
khimportiert <khimpaully@gmail.com>
2024-01-19 01:01:51 +0100

    init

---
+++ app.js
@@ -0,0 +1,16 @@
+const path = require('path')
+const pdf_extract = require('pdf-extract')
+
+console.log("Usage: node app.js the/path/tothe.pdf")
+const absolute_path_to_pdf = path.resolve(process.argv[2])
+if (absolute_path_to_pdf.includes(" ")) throw new Error("will fail for paths w spaces like "+absolute_path_to_pdf)
+
+const options = {
+    type: 'ocr', // perform ocr to get the text within the scanned image
+    ocr_flags: ['--psm 1', '-l deu'], // automatically detect page orientation
+    mode: 'simple',
+}
+const processor = pdf_extract(absolute_path_to_pdf, options, ()=>console.log("Starting…"))
+processor.on('complete', data => callback(null, data))
+processor.on('error', callback)
+function callback (error, data) { error ? console.error(error) : console.log(data.text_pages[0]) }
